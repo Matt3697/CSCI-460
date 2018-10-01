60415316fb1df7e165b7abe7bb96bae1eb2fa58e
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2018-10-01 09:37:28 -0600

    progress on Circular procedure

--- pa1/family_project/Family_name_1.java
+++ pa1/family_project/Family_name_1.java
@@ -8,8 +8,8 @@ public class Family_name_1 {
 	public static void main(String[] args) throws IOException {
 	    
 		Scanner scanner = null;
-		int system_time = 0;									//Variable to keep track of system time.
-		ArrayList<Integer> input = new ArrayList<Integer>();	//holds data from input.txt
+		int system_time = 0;									//Variable to keep track of system time in milliseconds.
+		ArrayList<Integer> input = new ArrayList<Integer>();	//holds data from input.txt in the form of Job# | Arrival_Time | Processing_Time
 		ArrayList<Job> jobs = new ArrayList<Job>();			//holds individual jobs from input.
 		ArrayList<Processor> processors = new ArrayList<Processor>(); //holds individual processors.
 		int std_no = 8788;									//last four of student id.
@@ -47,14 +47,31 @@ public class Family_name_1 {
 		//at any processor to run. When the job sequence is finished, measure the overall turn-around time. I.e the time duration 
 		//between the arrival of the first job and the finish of the last job.
 		//Test your program on randomly generated job sequences of 100 which arrive every 1ms.
-		int i = 0;
+		int i = 0; 
 		int j = 0;
-		while(i < jobs.size()){
-			if(jobs.get(i).get_arrival_time() == system_time) {
-				processors.get(j).onLoad_job(jobs.get(i));
+		int y = 0;
+		int processor_num = 0;
+		while(y < jobs.size()){
+			
+			System.out.println("System Time at iteration " + i + ":" + system_time);
+			if(jobs.get(y).get_arrival_time() == i) { //at a jobs arrival time	
+				System.out.println("Job# " + jobs.get(y).getJobNum() + ":" + jobs.get(y).get_processing_time());
+				if(j == 0) {									   //put the first job on processor 0
+					processors.get(j).onLoad_job(jobs.get(y));
+				}
+				else {	
+					System.out.println("using the else: " + jobs.get(y).getJobNum());//otherwise put jobs on processor (j+1)%k
+					processor_num = (j + 1) % k;
+					processors.get(processor_num).onLoad_job(jobs.get(y));
+				}
+				system_time += jobs.get(y).get_processing_time(); //add processing time to total system time.
+				j++;
+				y++;
 			}
-			system_time++;
+			system_time++; //assume that it takes 1ms to put each job at any processor. i.e add 1ms
+			i++;
 		}
+		System.out.println("Total System Time = " + system_time + "ms");
 		
 		PrintWriter writer = new PrintWriter("Family_name1.txt", "UTF-8");
 		writer.println("The first line");
--- pa1/family_project/Processor.java
+++ pa1/family_project/Processor.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Processor {
 	public int processor_id;
-	ArrayList<String> jobs = new ArrayList<String>();
+	ArrayList<Job> jobs = new ArrayList<Job>();
 	
 	public Processor(int k) {			//constructor for class, Processor.
 		processor_id = k;
@@ -12,11 +12,8 @@ public class Processor {
 	public int get_id() {				//returns the unique id for the processor.
 		return processor_id;
 	}
-	public void add_job(String job) {	//adds a job to the list of jobs on the processor.
-		jobs.add(job);
-	}
 	
-	public void onLoad_job(Job job) {
-		
+	public void onLoad_job(Job job) {    //adds job to list of jobs.
+		jobs.add(job);
 	}
 }
