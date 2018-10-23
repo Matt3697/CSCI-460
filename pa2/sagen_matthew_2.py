e93cfa5bdc47177350589c50050d802e924af236
Matthew Sagen <matthewsagen@matthews-mbp.home>
2018-10-23 17:52:05 -0600

    changed output functionality

--- pa2/sagen_matthew_2.py
+++ pa2/sagen_matthew_2.py
@@ -2,11 +2,13 @@
 Author: Matthew Sagen
 Date: 10/21/18
 CSCI-460: Programming Assignment 2
-
+To run the program, use: python3 sagen_matthew_2.py
+Output is written to sagen_matthew_2.txt
 '''
 import random
 import threading
 import time
+from numpy import savetxt
 
 class Node(object):
 
@@ -44,17 +46,18 @@ class DoublyLinkedList(object):
         curNode = self.head      #get the head of the list
         self.head = curNode.next #set the head of the list to the next node
         curNod = None            #remove the old head from the list
-        
 
-    def show(self, name):
+
+    def show(self, name, f):
         curNode = self.head
-        print_lock = threading.Lock()
-        print_lock.acquire(0)
+        #f = open("sagen_matthew_2.txt", "a")
         while curNode is not None:
-            print (name, '->', curNode.randomNum)
+            #print (name, '->', curNode.randomNum)
+            f.write(name+'->'+str(curNode.randomNum)+'\n')
             curNode = curNode.next
-        print ("*"*50)
-        print_lock.release()
+        #print ("*"*50)
+        f.write("*"*50+"\n")
+        #f.close()
 
 class Producer(threading.Thread):
 
@@ -66,52 +69,63 @@ class Producer(threading.Thread):
     def generateNode(self,randomNum):
         self.dblList.append(randomNum)
 
-    def generateMsg(self,name):
+    def generateMsg(self,name,f):
         msg = "Task completed:"
         print(msg, name)
+        f.write(msg+' '+name+'\n')
 
     def getNodeNum(self):
         return self.dblList.numNodes
 
-    def showList(self, name):
-        self.dblList.show(name)
+    def showList(self, name,f):
+        self.dblList.show(name,f)
 
     def run(self):
-        #while(True):
-        maxNodes = 30
-        name = threading.currentThread().getName()
-        var_lock = threading.Lock()
-        try:
-            var_lock.acquire()
-            if(name == 'p1'):
-                i = self.getNodeNum()
-                if(i < maxNodes):
-                    print("Before p1:")
-                    self.showList(name)
-                    oddRandomNum = random.randrange(1, 49+1, 2)
-                    self.generateNode(oddRandomNum)
-                    print("After p1:")
-                    self.showList(name)
-                else:
-                    self.generateMsg(name)
-                    #break;
-            elif(name == 'p2'):
-                i = self.getNodeNum()
-                if(i < maxNodes):
-                    print("Before p2:")
-                    self.showList(name)
-                    evenRandomNum = random.randrange(0, 49+1, 2)
-                    self.generateNode(evenRandomNum)
-                    print("After p2:")
-                    self.showList(name)
-                else:
-                    self.generateMsg(name)
-                    #break;
-            var_lock.release()
-            time.sleep(2)
-        except:
-            print(name, '-> Could not acquire Lock')
-            time.sleep(2)
+        while(True):
+            maxNodes = 30
+            name = threading.currentThread().getName()
+            var_lock = threading.Lock()
+            try:
+                var_lock.acquire()
+                f = open("sagen_matthew_2.txt", "a")
+                if(name == 'p1'):
+                    i = self.getNodeNum()
+                    if(i < maxNodes):
+                        #print("Before p1:")
+                        f.write("Before p1:\n")
+                        self.showList(name,f)
+                        oddRandomNum = random.randrange(1, 49+1, 2)
+                        self.generateNode(oddRandomNum)
+                        #print("After p1:")
+                        f.write("After p1:\n")
+                        self.showList(name,f)
+                    else:
+                        self.generateMsg(name,f)
+                        sys.exit()
+                        #break;
+                elif(name == 'p2'):
+                    i = self.getNodeNum()
+                    if(i < maxNodes):
+                        #print("Before p2:")
+                        f.write("Before p2:\n")
+                        self.showList(name,f)
+                        evenRandomNum = random.randrange(0, 49+1, 2)
+                        self.generateNode(evenRandomNum)
+                        #print("After p2:")
+                        f.write("After p2:\n")
+                        self.showList(name,f)
+                    else:
+                        self.generateMsg(name,f)
+                        sys.exit()
+                        #break;
+                f.close()
+                var_lock.release()
+                time.sleep(2)
+            except:
+                #print(name, '-> Could not acquire Lock')
+                f.write(name+': Could not acquire Lock.\n')
+                f.close()
+                time.sleep(2)
 class Consumer(threading.Thread):
     def __init__(self, dblList, name):
         super(Consumer, self).__init__()
@@ -121,69 +135,76 @@ class Consumer(threading.Thread):
     def deleteHead(self, node):
         self.dblList.remove(node)
 
-    def generateMsg(self, name):
+    def generateMsg(self, name,f):
         msg = "Task completed:"
         print(msg, name)
+        f.write(msg+' '+name+'\n')
 
     def getNodeNum(self):
         return self.dblList.numNodes
 
-    def showList(self,name):
-        self.dblList.show(name)
+    def showList(self,name,f):
+        self.dblList.show(name,f)
 
     def run(self):
-        #while(True):
-        name = threading.currentThread().getName()
-        var_lock = threading.Lock()
-        try:
-            var_lock.acquire()
-            if(name == 'c1'):
-                i = self.getNodeNum()
-                print("I: ", i)
-                if(i > 0 and i % 2 != 0):
-                    print("Before c1:")
-                    self.showList(name)
-                    try:
-                        print("hi")
+        while(True):
+            name = threading.currentThread().getName()
+            var_lock = threading.Lock()
+            try:
+                var_lock.acquire()
+                f = open("sagen_matthew_2.txt", "a")
+                if(name == 'c1'):
+                    i = self.getNodeNum()
+                    print("I: ", i)
+                    if(i > 0 and i % 2 != 0):
+                        #print("Before c1:")
+                        f.write("Before c1:\n")
+                        self.showList(name,f)
                         self.deleteHead(0)
-                    except:
-                        print('uhoahoh')
-                    print("After c1:")
-                    self.showList(name)
-                else:
-                    self.generateMsg(name)
-                    #break;
-            elif(name == 'c2'):
-                i = self.getNodeNum()
-                if(i > 0 and i % 2 == 0):
-                    print("Before c2:")
-                    self.showList(name)
-                    try:
+                        #print("After c1:")
+                        f.write("After c1:\n")
+                        self.showList(name,f)
+                    else:
+                        self.generateMsg(name,f)
+                        sys.exit()
+                        #break;
+                elif(name == 'c2'):
+                    i = self.getNodeNum()
+                    if(i > 0 and i % 2 == 0):
+                        #print("Before c2:")
+                        f.write("Before c2:\n")
+                        self.showList(name,f)
                         self.deleteHead(0)
-                    except:
-                        print('uhoahoh2')
-                    print("After c2:")
-                    self.showList(name)
-                else:
-                    self.generateMsg(name)
-                    #break;
-            var_lock.release()
-            time.sleep(2)
-        except:
-            print(name, '-> Could not acquire Lock.')
-            time.sleep(2)
-
+                        #print("After c2:")
+                        f.write("After c2:\n")
+                        self.showList(name,f)
+                    else:
+                        self.generateMsg(name,f)
+                        sys.exit()
+                        #break;
+                f.close()
+                var_lock.release()
+                time.sleep(2)
+            except:
+                #print(name, '-> Could not acquire Lock.')
+                f.write(name+': Could not acquire Lock.\n')
+                f.close()
+                time.sleep(2)
 
 class Main():
 
     def main():
+     f = open("sagen_matthew_2.txt", "w")
+     f.write("Starting threads...\n")
+     f.write("*"*50+"\n")
+     f.close()
      d = DoublyLinkedList()
      #initialize list with three nodes with a random int between 0 and 50
      for i in range(3):
          randomNum = random.randint(0,49)
          d.append(randomNum)
          i = i + 1
-     d.show('Init')
+     #d.show('Init')
      counter = 3
      p1 = Producer(d, name='p1')
      p2 = Producer(d, name='p2')
