c48916b1c349bd05c88d0e9437c1f13630cc0f30
Matthew Sagen <matthewsagen@matthews-mbp.home>
2018-11-27 20:04:29 -0700

    more work with threads

--- pa3/src/priorityInversion/Job.java
+++ pa3/src/priorityInversion/Job.java
@@ -1,22 +1,29 @@
 package priorityInversion;
+
 /*
 * Author: Matthew Sagen
 * Date:   11/27/18
 * Programming Assignment 3: Priority Inversion
 */
 public class Job extends Thread {
-	int jobId, arrivalTime, j;
+	int jobId, arrivalTime, priorityLevel, runTime;
 	
-	public Job(int jobId, int arrivalTime, int j) {
+	public Job(int jobId, int arrivalTime, int priorityLevel) {
 		this.arrivalTime = arrivalTime;
-		this.j = j;
+		this.priorityLevel = priorityLevel;
 		this.jobId = jobId;
+		if(priorityLevel == 1 || priorityLevel == 3) {
+			this.runTime = 3;
+		}
+		else {
+			this.runTime = 10;
+		}
 	}
 	
 	public int[] changeArray(int[] sharedBuffer) {
 		//convert the contents of the sharedBuffer to either 1's or 3's
 		for(int i = 0; i < sharedBuffer.length; i++) {
-			sharedBuffer[i] = j;
+			sharedBuffer[i] = priorityLevel;
 		}
 		
 		return sharedBuffer;
@@ -26,14 +33,48 @@ public class Job extends Thread {
 	public void run() { 
         try{ 
             // Displaying the thread that is running 
-            System.out.println ("Job " + jobId + " is running"); 
+            System.out.println ("Job " + jobId + " is waiting."); 
+            
         } 
         catch (Exception e) { 
             System.out.println ("Exception is caught"); 
         } 
     } 
 	
+	public void doJob(int[] sharedBuffer, int time) throws InterruptedException {
+		if(priorityLevel == 1 || priorityLevel == 3) {
+			try {
+				sharedBuffer = changeArray(sharedBuffer);
+				System.out.print("time " + time + ", T" + priorityLevel + ":");
+				for(int i = 0; i < sharedBuffer.length; i++) {
+					System.out.print(sharedBuffer[i]);
+				}
+				System.out.print(":T" + priorityLevel + "\n");
+			} 
+			finally{
+				Thread.sleep(runTime * 100);
+		     }
+		}
+		else {
+			System.out.print("time " + time + ", T" + priorityLevel);
+			for(int i = 0; i < runTime; i++) {
+				System.out.print("N");
+				Thread.sleep(runTime * 100);
+			}
+			System.out.print("T" + priorityLevel + "\n");
+		}
+		
+	}
 	public int getJobId() {
 		return jobId;
+	}
+	public int getPriorityLevel() {
+		return priorityLevel;
+	}
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
+	public int getRunTime() {
+		return runTime;
 	}
 }
--- pa3/src/priorityInversion/Sagen_Matthew.java
+++ pa3/src/priorityInversion/Sagen_Matthew.java
@@ -6,23 +6,54 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /*
  * Author: Matthew Sagen
  * Date:   11/27/18
  * Programming Assignment 3: Priority Inversion
+ * Program takes no arguments from standard input. Data is read into memory from src/priorityInversion/input.txt
+ * To run this program, simply run this file as a java application.
  */
 public class Sagen_Matthew {
 	
+
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		Lock queueLock = new ReentrantLock();
 		ArrayList<Job> jobs = get_inputFileJobArray();
-		int[] sharedBuffer = {0,0,0}; //. T1 shares a small buffer of length 3 with T3, with an initial value of < 0, 0, 0 > at time 0.
- 
-        for (int i=0; i<jobs.size(); i++) { 
-            jobs.get(i).start(); 
-        } 
+		ArrayList<Job> jobQueue = new ArrayList<Job>();
+		int[] sharedBuffer = {0,0,0}; // T1 shares a small buffer of length 3 with T3, with an initial value of < 0, 0, 0 > at time 0.
+		int time = 0; 				 // variable to simulate system time
 		
-        for (int i=0; i<jobs.size(); i++) { 
+		for(Job job: jobs) {
+	    		try {
+	    			job.start();
+	    			jobQueue.add(job);
+	    			Thread.sleep(10);
+	        }
+	        catch(Exception e) { 
+	            System.out.println ("Exception is caught"); 
+	        } 
+		}
+		
+        for(int i = time; i < 50; i++) {
+        		if(jobQueue.size() > 0 && i == jobQueue.get(0).getArrivalTime()) {
+        			queueLock.lock();
+            		try {
+            			jobQueue.get(0).doJob(sharedBuffer, time);
+                }
+                catch(Exception e) { 
+                    System.out.println ("Exception is caught"); 
+                } 
+            		queueLock.unlock();
+        			jobQueue.remove(0);
+        		}
+        		time++;
+        }
+       
+       
+        for (int i = 0; i < jobs.size(); i++) {
             try {
             		System.out.println("Job " + jobs.get(i).getJobId() + " is ending.");
 				jobs.get(i).join();
